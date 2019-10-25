24c1a4d777e4884224396903c4cfa2562809ca32
Willians Briceno <williansbriceno06@gmail.com>
2019-10-25 14:22:46 -0400

    Añadir carga de lista de productos desde la API

--- packages/page-productos/src/PageProductos.js
+++ packages/page-productos/src/PageProductos.js
@@ -8,17 +8,51 @@ export class PageProductos extends LitElement {
         padding: 25px;
         text-align: center;
       }
+
+      ul li {
+        font-weight: bold;
+        text-align: left;
+      }
+
+      ul li div {
+        font-weight: normal;
+      }
     `;
   }
 
   static get properties() {
     return {
+      products: { type: Array },
     };
+  }
+
+  constructor() {
+    super();
+    this.products = [];
+  }
+
+  async fetchProducts() {
+    const response = await fetch('https://ancient-mesa-25039.herokuapp.com/productos?filter[limit]=10',{
+      method: 'GET',
+      headers:{ 'Content-Type': 'application/json' }
+    });
+    this.products = await response.json();
+  }
+
+  firstUpdated() {
+    this.fetchProducts();
   }
 
   render() {
     return html`
       <h1>Productos</h1>
+      <ul>
+        ${this.products.map((product) => html`
+          <li>${product.nombre}
+            <div>${product.desc}</div>
+          </li><br>
+        `)}
+      </ul>
     `;
   }
 }
