f48791edb1556e5863b35492ff1cd5339c38a161
Willians Briceno <williansbriceno06@gmail.com>
2019-10-31 15:44:31 -0400

    Copiar archivos estáticos automáticamente a ./dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,22 @@
+import cpy from 'rollup-plugin-cpy';
 import { createDefaultConfig } from '@open-wc/building-rollup';
 
-// if you need to support IE11 use "modern-and-legacy-config" instead.
-// import { createCompatibilityConfig } from '@open-wc/building-rollup';
-// export default createCompatibilityConfig({ input: './index.html' });
+const config = createDefaultConfig({ input: './index.html' });
 
-export default createDefaultConfig({ input: './index.html' });
+export default [
+  // add plugin to the first config
+  {
+    ...config,
+    plugins: [
+      ...config.plugins,
+      cpy({
+        files: ['**/*.png', 'manifest.json', 'service-worker.js'],
+        dest: 'dist',
+        options: {
+          // parents makes sure to preserve the original folder structure
+          parents: true,
+        },
+      }),
+    ],
+  },
+];
